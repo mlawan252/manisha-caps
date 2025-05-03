dd970de1f95678d26e6ad012a911756d97dd3b21
Muhammad Lawan <mlawan252@gmail.com>
2025-05-03 09:47:08 +0100

    Fix CommonJS for Render deployment

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // server.js
 const jsonServer = require('json-server');
+const path = require('path');
+
 const server = jsonServer.create();
-const router = jsonServer.router('data/data.json'); // fixed path to match your folder
+const router = jsonServer.router(path.join(__dirname, 'data/data.json')); // points to data/data.json
 const middlewares = jsonServer.defaults();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 server.use(middlewares);
 server.use(router);
